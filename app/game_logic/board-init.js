a5af11a6ee974507c4574774841697bbdad329f4
koscelansky <koscelansky@live.com>
2018-03-04 11:07:40 +0100

    Use combine reducers to simplify state

---
+++ app/game_logic/board-init.js
@@ -0,0 +1,15 @@
+const boardInit = {
+  pieces: [
+    'WM', 'WM', 'WM', 'WM',
+    'WM', 'WM', 'WM', 'WM',
+    null, null, null, null,
+    null, null, null, null,
+    null, null, null, null,
+    null, null, null, null,
+    'BM', 'BM', 'BM', 'BM',
+    'BM', 'BM', 'BM', 'BM'
+  ],
+  turn: 'W'
+}
+
+export default boardInit
--- app/reducers.js
+++ app/reducers.js
@@ -1,22 +1,40 @@
+import { combineReducers } from 'redux'
+
 import { MOVE_PIECE, NEW_POSITION_FROM_FEN } from './actions.js'
-import initialState from './initial-state.js'
+import boardInitialState from './game_logic/board-init.js'
 import { performMove } from './game_logic/perform-move.js'
 import { fromFen } from './fen.js'
 
-export default function damaApp (state = initialState, action) {
+function board (state = boardInitialState, action) {
   switch (action.type) {
     case MOVE_PIECE: {
       const { move } = action
 
-      return Object.assign({}, state, { board: performMove(state.board, move) })
+      return performMove(state, move)
     }
     case NEW_POSITION_FROM_FEN: {
       const { fen } = action
 
-      return Object.assign({}, state, {board: fromFen(fen) || state.board})
+      return fromFen(fen) || state
     }
     default: {
       return state
     }
   }
 }
+
+function white (state = 'human', action) {
+  return state
+}
+
+function black (state = 'human', action) {
+  return state
+}
+
+const mainReducer = combineReducers({
+  board,
+  white,
+  black
+})
+
+export default mainReducer

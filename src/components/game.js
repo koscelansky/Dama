c2f43e3b6829387af302ef6a81f6158ecc49b0c8
koscelansky <koscelansky@live.com>
2020-09-11 21:12:06 +0200

    More switch to bootstrap

--- src/components/game.js
+++ src/components/game.js
@@ -1,38 +1,13 @@
 import React from 'react'
-import styled from 'styled-components/macro'
 import { useSelector } from 'react-redux'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 import MoveSelector from '../containers/move-selector'
 import Footer from '../containers/footer'
 import Player from '../containers/player'
 import WelcomeDlg from '../containers/welcome-dlg'
-
-const Layout = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-`
-
-const PlayerWrapper = styled.div`
-  padding: 2%;
-  width: 100%;
-  box-sizing: border-box;
-`
-
-const WhitePlayerWrapper = PlayerWrapper
-
-const BlackPlayerWrapper = styled(PlayerWrapper)`
-  justify-self: end;
-`
-
-const MoveSelectorWrapper = styled.div`
-  grid-column-start: 1;
-  grid-column-end: span 2;
-`
-
-const FooterWrapper = styled.div`
-  grid-column-start: 1;
-  grid-column-end: span 2;
-`
 
 const Game = () => {
   const showWelcome = useSelector(state => state.state === 'new')
@@ -40,20 +15,26 @@ const Game = () => {
   return (
     <>
       <WelcomeDlg show={showWelcome} />
-      <Layout>
-        <WhitePlayerWrapper>
-          <Player color='white' />
-        </WhitePlayerWrapper>
-        <BlackPlayerWrapper>
-          <Player color='black' right />
-        </BlackPlayerWrapper>
-        <MoveSelectorWrapper>
-          <MoveSelector />
-        </MoveSelectorWrapper>
-        <FooterWrapper>
-          <Footer />
-        </FooterWrapper>
-      </Layout>
+      <Container>
+        <Row>
+          <Col>
+            <Player color='white' />
+          </Col>
+          <Col>
+            <Player color='black' right />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <MoveSelector />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Footer />
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }

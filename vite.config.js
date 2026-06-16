36c93cf199917949a22f6c8836bcd75456569bb9
Peter Koscelansky <koscelansky@live.com>
2026-06-16 22:37:48 +0200

    added names to styled compoments for better debugging

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            { displayName: true, fileName: true },
+          ],
+        ],
+      },
+    }),
+  ],
   server: {
     host: '127.0.0.1',
     port: 3000
